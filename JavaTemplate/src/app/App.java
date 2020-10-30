2d3a69d92e3dc2084af983156edf5cb972848920
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-10-30 17:06:34 +0000

    Created Assignment

--- JavaTemplate/src/app/App.java
+++ JavaTemplate/src/app/App.java
@@ -1,7 +1,32 @@
+/*
+    ARRAYS PRACTICE
+    
+    Create a program that asks the user to enter a planet.
+    After the user enters a name of a planet, the program should print out that planet's position relative to the sun.
+    You may only use one if statement and one for loop to complete this practice problem.
+    When finished with this practice, move on to the assignment "ArraysChallenge"
+
+    EXAMPLE:
+
+    >Please enter a planet
+    >Earth
+    >Earth is position 3 from the sun
+
+    EXAMPLE:
+
+    >Please enter a planet
+    >Jupiter
+    >Jupiter is position 5 from the sun
+
+    
+*/
+
+
 package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        
+
     }
 }
